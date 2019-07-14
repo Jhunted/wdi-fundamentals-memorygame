20fad902a5085c51bbf403dcf24d2c7136f7fc1f
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-07-14 14:38:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,34 @@
 
 
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
 
+function checkForMatch()
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 	}
-	else{alert("Sorry, try again.")};
-
 }
+
+
+function flipCard(cardId)
+{
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) 
+	{
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{
+			alert ("You found a match!");
+		}
+		else{alert("Sorry, try again.")};
+	}
+}
+
+flipCard(0);
+flipCard(2);
